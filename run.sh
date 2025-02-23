cc7228c35060aefa324df92f3a43e6446dc1d430
Zhangqian0616 <147727397+Zhangqian0616@users.noreply.github.com>
2025-02-23 23:49:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Entropy decrease fast but maintaining compared to other --ema=0.95
-python main.py --ema_baseline_decay=0.9 --shared_initial_step=150 --reward_c=800 &
+python main.py --ema_baseline_decay=0.9 --shared_initial_step=150 --reward_c=800 --data_path=path of NAS-Bench-101
 sleep 2
 
 # BAD # live until 5k entropy was high so shared_loss was unstable
@@ -22,5 +22,5 @@ sleep 2
 #python main.py --ema_baseline_decay=0.92 --shared_initial_step=150 --reward_c=80 --ppl_square=True &
 #sleep 2
 
-python main.py --ema_baseline_decay=0.95 --shared_initial_step=150 --reward_c=80 --ppl_square=True &
+python main.py --ema_baseline_decay=0.95 --shared_initial_step=150 --reward_c=80 --ppl_square=True --data_path=path of NAS-Bench-201
 sleep 2
