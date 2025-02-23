5753f5b2b5b590f522849306c6546318105572f3
Zhangqian0616 <147727397+Zhangqian0616@users.noreply.github.com>
2025-02-23 23:47:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Entropy decrease fast but maintaining compared to other --ema=0.95
-#python main.py --ema_baseline_decay=0.9 --shared_initial_step=150 --reward_c=800 &
-#sleep 2
+python main.py --ema_baseline_decay=0.9 --shared_initial_step=150 --reward_c=800 &
+sleep 2
 
 # BAD # live until 5k entropy was high so shared_loss was unstable
 #python main.py --ema_baseline_decay=0.9 --shared_initial_step=150 --reward_c=80 &
@@ -17,10 +17,10 @@
 #sleep 2
 
 #python main.py --ema_baseline_decay=0.9 --shared_initial_step=150 --reward_c=80 --ppl_square=True &
-sleep 2
+#sleep 2
 
 #python main.py --ema_baseline_decay=0.92 --shared_initial_step=150 --reward_c=80 --ppl_square=True &
-sleep 2
+#sleep 2
 
 python main.py --ema_baseline_decay=0.95 --shared_initial_step=150 --reward_c=80 --ppl_square=True &
 sleep 2
